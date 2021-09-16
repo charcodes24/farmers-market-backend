5b0ab24abf7e5ba48aef851726bfb2eb658dd65b
charcodes24 <charcodes24@gmail.com>
2021-09-16 14:24:00 -0700

    Generated Sessions controller and added '/login' route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show]
   end
   resources :customers
+
+
+  #signing customer or vendor in 
+  get '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
