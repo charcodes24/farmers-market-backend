4aa89ba84cfb0e4f3013e33a2afdd873ac68ead3
charcodes24 <charcodes24@gmail.com>
2021-09-16 06:14:27 -0700

    Fixed config in cors.rb file implemented response logic for GET fetch of all vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,7 @@
 class VendorsController < ApplicationController
+
+    def index
+        vendors = Vendor.all
+        render json: vendors
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :order_items
   resources :items
   resources :orders
-  resources :vendors
+  resources :vendors do 
+    resources :items, only: [:index, :show]
+  end
   resources :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
