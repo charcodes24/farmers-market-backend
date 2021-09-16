7c718f9c6e9cfb7d8e63abfeafaadccc6fd486b8
charcodes24 <charcodes24@gmail.com>
2021-09-16 14:38:43 -0700

    Created sessions#create for customer OR vendor login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+    def create 
+    user = Customer.find_by(username: params[:username]) || Vendor.find_by(username: params[:username])
+    session
+    if user&.authenticate(params[:password])
+        if (user.instance_of? Vendor)
+          session[:vendor_id] = user.id
+        else 
+          session[:customer_id] = user.id
+        end
+        render json: user 
+    else 
+        render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
 
   #signing customer or vendor in 
-  get '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
