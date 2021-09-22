54290f5057c3d5dd7b216f55d00f24b38c410e42
charcodes24 <charcodes24@gmail.com>
2021-09-22 14:24:15 -0700

    Route and action for creating orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+    
+    def create
+        byebug
+        order = Order.create!(order_params)
+        render json: order
+    end
+
+    private 
+
+    def order_params
+        params.permit(:subtotal, :customer_id, item_ids: [])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :order_items
   resources :items, only: [:update, :destroy]
-  resources :orders
   resources :vendors do
     resources :items, only: [:index, :show]
   end
@@ -26,5 +25,8 @@ Rails.application.routes.draw do
 
   #keeping customer and vendor logged in
   get '/loggedin', to: 'login#show'
+
+  #creating an order 
+  post '/order', to: 'orders#create'
 
 end
