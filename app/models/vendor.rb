d0d2f54a5d43df08481808d61d27a559e2926400
charcodes24 <charcodes24@gmail.com>
2021-10-04 16:25:58 -0700

    Added validations to Vendor model for vendor signup

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,12 @@
 class Vendor < ApplicationRecord
     has_many :items 
 
+
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :username, length: { maximum: 10 }
+    validates :username, presence: true 
+    validates :username, uniqueness: true 
+    
     has_secure_password
 end
