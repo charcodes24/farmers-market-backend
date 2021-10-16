d5d0fce52630878fd8b043d66018be264a02471d
charcodes24 <charcodes24@gmail.com>
2021-10-16 08:11:35 -0700

    SUpdate vendor mailer and customer order notification to quantify items in orders

--- app/mailers/vendor_mailer.rb
+++ app/mailers/vendor_mailer.rb
@@ -2,7 +2,6 @@ class VendorMailer < ApplicationMailer
 
     def welcome_vendor
         @vendor = params[:vendor]
-        byebug
         @url = 'http://localhost:3001/login'
         mail(to: @vendor.email, subject: 'Welcome!')
     end
