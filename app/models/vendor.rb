606d62d20ac90422b87d509c87445198741934ca
charcodes24 <charcodes24@gmail.com>
2021-11-03 15:36:53 -0700

    Removed validations from Customer and Vendor models in response to refactoring frontend repo with useForm. Added email validation with regex (preliminary changes)

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,11 +2,13 @@ class Vendor < ApplicationRecord
     has_many :items 
 
 
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :email, presence: true
-    validates :username, presence: true 
-    validates :username, uniqueness: true 
+    # validates :name, presence: true
+    # validates :description, presence: true
+    # validates :email, presence: true
+    # validates :username, presence: true 
+    # validates :username, uniqueness: true 
+
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
     has_secure_password
 end
